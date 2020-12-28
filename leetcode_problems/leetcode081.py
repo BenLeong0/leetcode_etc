feddca4ccbebddf49c851d3a65a4f601e841ec80
= <benleong0@gmail.com>
2020-12-28 16:06:29 +0000

    81

--- leetcode_problems/leetcode081.py
+++ leetcode_problems/leetcode081.py
@@ -1,23 +1,15 @@
 def f(nums, target):
-    l = 0
-    r = len(nums)
-
     if not nums:
     	return False
 
-    while r-l > 1:
-        if nums[(l+r)//2] >= nums[l]:
-            l = (l+r)//2
-        else:
-            r = (l+r)//2
-    piv = r
-    print(piv)
-    nums = nums[piv:] + nums[:piv]
+    for i in range(len(nums)-1):
+        if nums[i] > nums[i+1]:
+            nums = nums[i+1:] + nums[:i+1]
+            break
 
     l = 0
     r = len(nums)
     while True:
-        print(nums,l,r)
         if nums[(l+r)//2] == target:
             return True
         elif r - l == 1:
