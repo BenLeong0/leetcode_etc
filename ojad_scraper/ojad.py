b3daa6df6720e4d3957628e4b51df34ece97b3f6
= <benleong0@gmail.com>
2021-07-07 21:12:34 +0100

    keep soupness

--- ojad_scraper/ojad.py
+++ ojad_scraper/ojad.py
@@ -17,23 +17,24 @@ midashi_word = '<p class="midashi_word">.*?</p>'
 def get_html_sections(words):
     query_param = '%20'.join(words)
     url = f"http://www.gavo.t.u-tokyo.ac.jp/ojad/search/index/word:{query_param}"
-    html = requests.post(url).text
 
-    get_section_regex = '<tr id="word.*?<\/tr>'
-    return re.findall(get_section_regex, html, re.S)
+    html = requests.post(url).text
+    soup = BeautifulSoup(html, 'html.parser')
+
+    sections = soup.findAll('tr', id=lambda x: x and x.startswith('word_'))
+    return sections
 
 
 def get_writings(section):
-    get_midashi_regex = '<p class="midashi_word">.*?</p>'
-    midashi = re.search(get_midashi_regex, section)[0]
-    writings = re.search('>.*?<', midashi)[0][1:-1].split('・')
-    filtered = [re.match('[^\[]*', writing)[0] for writing in writings]
+    # soup = BeautifulSoup(section, 'html.parser')
+    midashi = section.findNext('p', class_='midashi_word').text
+    writings = midashi.split('・')
+    filtered = [re.search('[^\[]*', writing).group() for writing in writings]
     print(filtered)
 
 
 def get_accents(section):
-    soup = BeautifulSoup(section, 'html.parser')
-    data = soup.find('td', class_='katsuyo_jisho_js').find('span', class_='accented_word')
+    data = section.find('td', class_='katsuyo_jisho_js').find('span', class_='accented_word')
     if data:
         contents = data.contents
         classes = [span['class'] for span in contents]
