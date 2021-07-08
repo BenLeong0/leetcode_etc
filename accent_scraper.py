723b2792d43ccf6c12c96490650605ed227f0c09
benleong0 <benleong@urbanjungle.com>
2021-07-08 13:17:10 +0100

    inside single function

--- accent_scraper.py
+++ accent_scraper.py
@@ -19,29 +19,34 @@ words = [
     'する'
 ]
 
-accent_dict = collections.defaultdict(dict)
 
-def call_script(src):
-    if src == "ojad":
-        for (word, accents) in OJAD(words).get_accents():
-            accent_dict[src][word] = accents
-    elif src == "suzuki":
-        for (word, accents) in Suzuki(words).get_accents():
-            accent_dict[src][word] = accents
-    elif src == "wadoku":
-        for (word, accents) in Wadoku(words).get_accents():
-            accent_dict[src][word] = accents
+def get_full_accent_dict(words):
+    accent_dict = collections.defaultdict(dict)
 
-t1 = Thread(target=call_script, args=['ojad'])
-t2 = Thread(target=call_script, args=['suzuki'])
-t3 = Thread(target=call_script, args=['wadoku'])
+    def call_script(src):
+        if src == "ojad":
+            for (word, accents) in OJAD(words).get_accents():
+                accent_dict[src][word] = accents
+        elif src == "suzuki":
+            for (word, accents) in Suzuki(words).get_accents():
+                accent_dict[src][word] = accents
+        elif src == "wadoku":
+            for (word, accents) in Wadoku(words).get_accents():
+                accent_dict[src][word] = accents
 
-t1.start()
-t2.start()
-t3.start()
+    t1 = Thread(target=call_script, args=['ojad'])
+    t2 = Thread(target=call_script, args=['suzuki'])
+    t3 = Thread(target=call_script, args=['wadoku'])
 
-t1.join()
-t2.join()
-t3.join()
+    t1.start()
+    t2.start()
+    t3.start()
 
-print(accent_dict)
+    t1.join()
+    t2.join()
+    t3.join()
+
+    print(accent_dict)
+
+
+get_full_accent_dict(words)
