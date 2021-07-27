ce20e5e01f9549e9c23a895fde7972588ec1670c
benleong0 <benleong@urbanjungle.com>
2021-07-27 09:49:15 +0100

    .

--- python_fiddling/string_formatting.py
+++ python_fiddling/string_formatting.py
@@ -1,11 +1,11 @@
 class MyClass:
-    def __format__(self, format_spec) -> str:
-        return f"Did you know: {format_spec=!r}"
+    def __format__(self, format_spec, x=None) -> str:
+        return f"Did you know{x}: {format_spec=!r}"
 
 
 def main():
     x = 5
-    print(f'{MyClass():x}')
+    print(f'{MyClass():%x}')
 
 
 if __name__ == '__main__':
