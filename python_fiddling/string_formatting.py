1e3f41a82b7fedf26f653b32462e5d00046f5acc
benleong0 <benleong@urbanjungle.com>
2021-07-27 09:38:19 +0100

    string formatting

--- python_fiddling/string_formatting.py
+++ python_fiddling/string_formatting.py
@@ -1,7 +1,12 @@
+class MyClass:
+    def __format__(self, format_spec) -> str:
+        return f"Did you know: {format_spec=!r}"
+
+
 def main():
-    return
+    x = 5
+    print(f'{MyClass():x}')
 
 
-
-if __name__ == 'main':
+if __name__ == '__main__':
     main()
