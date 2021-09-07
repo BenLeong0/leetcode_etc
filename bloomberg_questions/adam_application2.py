2222c262aabccf69ad749fed6f20d7326ff0d864
benleong0 <benleong@urbanjungle.com>
2021-09-07 11:35:16 +0100

    adamapplication method

--- bloomberg_questions/adam_application2.py
+++ bloomberg_questions/adam_application2.py
@@ -9,7 +9,7 @@ def largestGood(binString=binString):
         currCount = 0
         currStart = 0
         substrings = []
-        
+
         for i in range(len(binString)):
             if binString[i] == '1':
                 currCount += 1
@@ -19,9 +19,11 @@ def largestGood(binString=binString):
                     res = rec_sort(binString[currStart+1:i])
                     substrings.append('1' + res + '0')
                     currStart = i+1
-        max_length = len(max(substrings, key=len))
-        print(substrings)
-        substrings.sort(key=lambda x: int(x.ljust(max_length, '0'),2), reverse=True)
+        ## Submitted method (wrong)
+        # max_length = len(max(substrings, key=len))
+        # print(substrings)
+        # substrings.sort(key=lambda x: int(x.ljust(max_length, '0'),2), reverse=True)
+        substrings.sort(reverse=True)
         return ''.join(substrings)
     return rec_sort()
 
