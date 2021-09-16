976dea267f2a083503db69d5fde0fb32a2c10676
benleong0 <benleong@urbanjungle.com>
2021-09-16 11:33:19 +0100

    use none + comments

--- uj_interview_answers/numbers_test.py
+++ uj_interview_answers/numbers_test.py
@@ -21,10 +21,11 @@ def max_number(filename):
     return max_number
 
 assert max_number(FILENAME) == MAX_NUM
+print("max_number successful!")
 
 
 def max_row(filename):
-    largest_sum = -math.inf
+    largest_sum = None
     largest_row = []
 
     file = open(filename)
@@ -35,13 +36,14 @@ def max_row(filename):
         parsed_line = map(int, line)
         line_sum = sum(parsed_line)
 
-        if line_sum > largest_sum:
+        if largest_sum is None or line_sum > largest_sum:
             largest_sum = line_sum
             largest_row = line
 
     return largest_row
 
 assert max_row(FILENAME) == MAX_ROW
+print("max_row successful!")
 
 
 # CULTURA MÁXIMA
@@ -53,6 +55,7 @@ def max_number_csv(filename):
     # return max([max(map(int, row)) for row in csv.reader(open(filename))]
 
 assert max_number_csv(FILENAME) == MAX_NUM
+print("max_number_csv successful!")
 
 
 def max_row_comprehension(filename):
@@ -62,3 +65,4 @@ def max_row_comprehension(filename):
     # return max(csv.reader(open(filename)), key=lambda line: sum(map(int, line)))
 
 assert max_row_comprehension(FILENAME) == MAX_ROW
+print("max_row_comprehension successful!")
