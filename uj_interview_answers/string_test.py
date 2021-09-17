bd988c5d9dcf8338824085e3823bc5a7d6db6228
benleong0 <benleong@urbanjungle.com>
2021-09-17 09:48:32 +0100

    reorder string

--- uj_interview_answers/string_test.py
+++ uj_interview_answers/string_test.py
@@ -4,20 +4,14 @@ TEST_STRING = 'they mostly come at night. Mostly.'
 def sort_word_excl_dupes(s):
     return ''.join(sorted(set(s)))
 
-
-def test_sort_word_excl_dupes():
-    assert sort_word_excl_dupes(TEST_STRING) == ' .Maceghilmnosty'
-    assert sort_word_excl_dupes('') == ''
-
-test_sort_word_excl_dupes()
+assert sort_word_excl_dupes(TEST_STRING) == ' .Maceghilmnosty'
+assert sort_word_excl_dupes('') == ''
+print("sort_word_excl_dupes successful!")
 
 
 def reverse_sentence(s):
     return s.split(' ')[::-1] if s else []
 
-
-def test_reverse_sentence():
-    assert reverse_sentence(TEST_STRING) == ['Mostly.', 'night.', 'at', 'come', 'mostly', 'they']
-    assert reverse_sentence('') == []
-
-test_reverse_sentence()
+assert reverse_sentence(TEST_STRING) == ['Mostly.', 'night.', 'at', 'come', 'mostly', 'they']
+assert reverse_sentence('') == []
+print("reverse_sentence successful!")
