4abfab8cdc4078dd09d048b55a6eeb0f06496fb8
benleong0 <benleong@urbanjungle.com>
2021-10-12 15:16:18 +0100

    <=

--- uj_interview_answers/javascript_questions.js
+++ uj_interview_answers/javascript_questions.js
@@ -4,6 +4,7 @@ const TEST_STRING = 'they mostly come at night. Mostly.';
 ///////////////
 
 const threeFiveMultiplesSum = (n) => [...Array(n).keys()].reduce((a,b)=>(b%3&&b%5)?a:a+b);
+const multiplesSum = (n, factors) => [...Array(n).keys()].reduce((a,b)=>(b%3&&b%5)?a:a+b);
 
 console.log(threeFiveMultiplesSum(10))    // 23
 console.log(threeFiveMultiplesSum(1000))  // 233168
@@ -12,14 +13,14 @@ console.log(threeFiveMultiplesSum(1000))  // 233168
 
 const fibSum = (n) => {
   var stack = [0,1];
-  while (stack[stack.length-1] < n) {stack.push(stack[stack.length-1]+stack[stack.length-2])};
+  while (stack[stack.length-1] <= n) {stack.push(stack[stack.length-1]+stack[stack.length-2])};
   return stack.slice(0,stack.length-1).reduce((a,b)=>(b%2===0)?a+b:a);
 }
 
 const fibSumIterative = (n) => {
   queue = [ 0, 1 ];
   var sum = 0;
-  while (queue[1] < n) {
+  while (queue[1] <= n) {
     if (queue[1] % 2 === 0) {sum += queue[1]};
     queue.push(queue[1] + queue.shift());
   }
