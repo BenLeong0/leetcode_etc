a8b7994492d8dbd326f49433c141511510416e3c
benleong0 <benleong@urbanjungle.com>
2021-10-12 12:26:29 +0100

    pandas

--- uj_interview_answers/numbers_test_pandas.py
+++ uj_interview_answers/numbers_test_pandas.py
@@ -26,3 +26,12 @@ print(max_number("blank.csv"))
 
 print(max_row("data_numbers.csv"))
 print(max_row("blank.csv"))
+
+csv_data = pd.read_csv("data_numbers.csv")
+largest_row_sum = -float("inf")
+for row in csv_data.iterrows():
+    current_row_sum = row.sum(axis=1)
+    if current_row_sum > largest_row_sum:
+        largest_row_sum = current_row_sum
+        largest_row = row
+print(largest_row)
